13838ee1e696cbd9122d28861a08b251b85c9a86
Valeria <lerik13@gmail.com>
2024-05-23 16:56:24 -0600

    move NavBar to App to avoid prop drilling, using composition

--- movies-project/src/App.js
+++ movies-project/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import './index.css';
-import { NavBar } from "./components/NavBar.js";
+//import { NavBar } from "./components/NavBar.js";
 import { ListBox } from './components/ListBox.js';
 import { WatchedBox } from './components/WatchedBox.js';
 import { tempMovieData } from './data.js';
@@ -10,7 +10,10 @@ function App() {
 
 	return (
 		<div>
-			<NavBar movies={movies} />
+			<NavBar>
+				<Search />
+				<NumResults movies={movies} />
+			</NavBar>
 			<main className="main">
 				<ListBox movies={movies} />
 				<WatchedBox />
@@ -19,6 +22,45 @@ function App() {
 	);
 }
 
+function NavBar({ children }) {
+	return (
+		<nav className="nav-bar">
+			<Logo />
+			{children}
+		</nav>
+	)
+}
 
+function Logo() {
+	return (
+		<div className="logo">
+			<span role="img">😎</span>
+			<h1>Favorite Movies</h1>
+			<span role="img">🎥</span>
+		</div>
+	)
+}
+
+function Search() {
+	const [query, setQuery] = useState("");
+
+	return (
+		<input
+			className="search"
+			type="text"
+			placeholder="Search ..."
+			value={query}
+			onChange={(e) => setQuery(e.target.value)}
+		/>
+	)
+}
+
+function NumResults({ movies }) {
+	return (
+		<p className="num-results">
+			Found <strong>{movies.length}</strong> results
+		</p>
+	)
+}
 
 export default App;
