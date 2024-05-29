23cc8ccbeeb3066d25bb4e713be1bd987edfd34f
Valeria <lerik13@gmail.com>
2024-05-29 11:41:51 -0600

    added Readme files

--- movies-project/src/components/MovieDetails.js
+++ movies-project/src/components/MovieDetails.js
@@ -1,5 +1,7 @@
+import { useEffect } from "react"
 
 const MovieDetails = ({ selectedId, onCloseMovie }) => {
+
 	return (
 		<div className="details">
 			<button className="btn-back" onClick={onCloseMovie}>&larr;</button>
