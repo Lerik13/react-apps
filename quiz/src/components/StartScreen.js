135bf2b07c85f05a4528f31a48688112e64d707c
Valeria <lerik13@gmail.com>
2024-06-05 12:56:34 -0600

    Quiz: + Question component, save current question number in state.index

--- quiz/src/components/StartScreen.js
+++ quiz/src/components/StartScreen.js
@@ -1,9 +1,14 @@
-const StartScreen = ({ numQuestions }) => {
+const StartScreen = ({ numQuestions, dispatch }) => {
 	return (
 		<div className="start">
 			<h2>Welcome to Animal Quiz!</h2>
 			<h3>{numQuestions} questions to test your knowledge about our live life in the planet</h3>
-			<button className="btn btn-ui">Let's start</button>
+			<button
+				className="btn btn-ui"
+				onClick={() => dispatch({ type: "start" })}
+			>
+				Let's start
+			</button>
 		</div>
 	);
 }
