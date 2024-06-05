077b60d727220befb0f82e6ee1222aed8fbab0f2
Valeria <lerik13@gmail.com>
2024-06-05 13:24:33 -0600

    Quiz: Question component > increase points if the answer was correct

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -12,6 +12,7 @@ const initialState = {
 	status: 'loading',
 	index: 0,
 	answer: null,
+	points: 0,
 }
 
 function reducer(state, action) {
@@ -26,8 +27,17 @@ function reducer(state, action) {
 			return {...state, status: "error" }
 		case 'start': 
 			return {...state, status: "active" }
-		case 'newAnswer': 
-			return {...state, answer: action.payload }
+		case 'newAnswer':
+			const question = state.questions.at(state.index);
+
+			return {
+				...state,
+				answer: action.payload,
+				points:
+					action.payload === question.correctOption
+						? state.points + question.points
+						: state.points,
+			}
 		default: 
 			throw new Error("Action unknown");
 	}
@@ -35,7 +45,7 @@ function reducer(state, action) {
 
 export default function App() {
 	const [state, dispatch] = useReducer(reducer, initialState);
-	const { questions, status, index, answer } = state;
+	const { questions, status, index, answer, points } = state;
 	const numQuestions = questions.length;
 
 	useEffect(() => {
