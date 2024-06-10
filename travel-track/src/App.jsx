ff1a79c382c63b142375820146135adbe344e248
Valeria <lerik13@gmail.com>
2024-06-10 17:31:03 -0600

    travel-track: CityList, CityItem, Message component

--- travel-track/src/App.jsx
+++ travel-track/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -5,10 +6,31 @@ import HomePage from './pages/HomePage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+import CityList from './components/CityList';
+
+const BASE_URL = 'http://localhost:9000';
 
 const App = () => {
-	return (
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
+	useEffect(() => {
+		async function fetchCities() {
+			try {
+				setIsLoading(true);
+				const res = await fetch(`${BASE_URL}/cities`);
+				const data = await res.json();
+				setCities(data);
+			} catch(err) {
+				console.log(err.message)
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		fetchCities();
+	}, []);
+
+	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
@@ -16,8 +38,8 @@ const App = () => {
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route index element={<p>List</p>} />
-					<Route path="cities" element={<p>List of cities</p>} />
+					<Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
 					<Route path="countries" element={<p>List of countries</p>} />
 					<Route path="form" element={<p>Form</p>} />
 				</Route>
