349056a0b867c64a19ce29a75c970b6ab441a0a6
Valeria <lerik13@gmail.com>
2024-06-18 12:57:09 -0600

    City component, dynamic routes with URL params

--- travel-track/src/App.jsx
+++ travel-track/src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -40,9 +41,9 @@ const App = () => {
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
 					<Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-					<Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
 					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-					<Route path="countries" element={<p>List of countries</p>} />
+					<Route path="cities/:id" element={<City />} />
+					<Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
 					<Route path="form" element={<p>Form</p>} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
