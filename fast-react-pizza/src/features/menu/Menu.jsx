a938cd9efe3dbe012d5fd28974c0bd82724d5b35
Valeria <lerik13@gmail.com>
2024-12-05 15:04:02 -0700

    fast-react-pizza: Fetching data Menu with ReactRouter 'Loaders'

--- fast-react-pizza/src/features/menu/Menu.jsx
+++ fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,9 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
+	const menu = useLoaderData();
+	console.log(menu);
+
 	return (
-		<div>
-			Menu
-		</div>
+		<ul>
+			{menu.map(pizza => (
+				<MenuItem pizza={pizza} key={pizza.id} />
+			))}
+		</ul>
 	)
 }
 
-export default Menu
+export async function loader() {
+	const menu = await getMenu();
+	return menu;
+}
+
+export default Menu;
