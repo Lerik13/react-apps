46b4d2bbbf2e6bece712c2564b7ae3e6d35a24a4
Valeria <lerik13@gmail.com>
2025-01-01 18:19:35 -0700

    fast-react-pizza: install tailwind, prettier

--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function CartOverview() {
-	return (
-		<div>
-			<p>
-				<span>30 pizzas</span>
-				<span>$100.25</span>
-			</p>
-			<Link to="/cart">Open cart &rarr;</Link>
-		</div>
-	)
+  return (
+    <div className="bg-stone-800 uppercase text-stone-200">
+      <p className="font-semibold text-stone-300">
+        <span>30 pizzas</span>
+        <span>$100.25</span>
+      </p>
+      <Link to="/cart">Open cart &rarr;</Link>
+    </div>
+  );
 }
 
-export default CartOverview
+export default CartOverview;
