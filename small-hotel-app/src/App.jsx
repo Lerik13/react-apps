72fea6209a26228e405e7a916fd5cbafa200e7ca
Valeria <lerik13@gmail.com>
2025-01-07 13:30:01 -0700

    small-hotel-app: reusable UI component Row, Heading

--- small-hotel-app/src/App.jsx
+++ small-hotel-app/src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from './styles/GlobalStyles'
 import Button from './ui/Button'
 import Input from './ui/Input'
 import Heading from './ui/Heading'
+import Row from './ui/Row'
 
 const StyledApp = styled.div`
-  background-color: lightsteelblue;
+  background-color: whitesmoke;
   padding: 20px;
   height: 100dvh;
 `
@@ -15,14 +16,24 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Hello</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert('Check In')}>Check in</Button>
+              <Button onClick={() => alert('Check out')}>Check out</Button>
+            </div>
+          </Row>
 
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert('Check In')}>Check in</Button>
-        <Button onClick={() => alert('Check out')}>Check out</Button>
-
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form action="">
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   )
