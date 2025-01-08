64a34f8eabe76b1918d4a534170642e2b9038d49
Valeria <lerik13@gmail.com>
2025-01-08 10:56:07 -0700

    small-hotel-app: connecting Supabase

--- small-hotel-app/src/pages/Cabins.jsx
+++ small-hotel-app/src/pages/Cabins.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from 'react'
+import Row from '../ui/Row'
+import Heading from '../ui/Heading'
+import { getCabins } from '../services/apiCabins'
+
 function Cabins() {
-  return <div>Cabins</div>
+  useEffect(() => {
+    getCabins().then((data) => console.log(data))
+  }, [])
+
+  return (
+    <Row type="horizontal">
+      <Heading as="h1">All Cabins</Heading>
+    </Row>
+  )
 }
 
 export default Cabins
