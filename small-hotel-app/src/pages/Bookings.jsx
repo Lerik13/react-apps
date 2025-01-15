4142a15de4d46150db22e9eda602f4c865de11bc
Valeria <lerik13@gmail.com>
2025-01-15 10:04:18 -0700

    small-hotel-app: Bookings page, + fetch bookings

--- small-hotel-app/src/pages/Bookings.jsx
+++ small-hotel-app/src/pages/Bookings.jsx
@@ -1,5 +1,18 @@
+import Row from '../ui/Row'
+import Heading from '../ui/Heading'
+import BookingTable from '../features/bookings/BookingTable'
+
 function Bookings() {
-  return <div>Bookings</div>
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All bookings</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <BookingTable />
+    </>
+  )
 }
 
 export default Bookings
