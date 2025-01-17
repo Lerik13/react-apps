056aaabd95a28efdfa3ddba15e5706a71b5db19b
Valeria <lerik13@gmail.com>
2025-01-17 16:33:25 -0700

    small-hotel-app: Logout User

---
+++ small-hotel-app/src/features/authentication/Logout.jsx
@@ -0,0 +1,17 @@
+import { HiArrowRightEndOnRectangle } from 'react-icons/hi2'
+import ButtonIcon from '../../ui/ButtonIcon'
+import SpinnerMini from '../../ui/SpinnerMini'
+import { useLogout } from './useLogout'
+
+function Logout() {
+  const { logout, isLoading } = useLogout()
+
+  return (
+    <ButtonIcon disabled={isLoading} onClick={logout}>
+      {!isLoading ? <HiArrowRightEndOnRectangle /> : <SpinnerMini />}
+      Logout
+    </ButtonIcon>
+  )
+}
+
+export default Logout
